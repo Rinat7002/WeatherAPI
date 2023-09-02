5d9275119760a54f78800f2e44347da59e1970e5
Rinat <89158137+Rinat7002@users.noreply.github.com>
2023-09-02 22:46:03 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+from flask import Flask, request
+from flask_restful import Api, Resource
+import asyncio
+import weather
+
+app = Flask(__name__)
+api = Api()
+
+
+async def main(city):
+    Weather = await weather.getweather(city)
+    weather2 = {"City": city, "Weather": Weather}
+    return weather2
+
+
+class Main(Resource):
+    def get(self):
+        city = request.args.get('city')
+        result = asyncio.run(main(city))
+        return result
+
+
+api.add_resource(Main, '/weather')
+api.init_app(app)
+
+if __name__ == "__main__":
+    app.run(debug=True, port=3000, host="127.0.0.1")
+
+# http://127.0.0.1:3000/weather?city=Ufa
